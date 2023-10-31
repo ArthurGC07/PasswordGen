87880da4d6caa16078ba3cef31485bc4724bf1b8
Arthur Gomes de Carvalho <122561395+ArthurGC07@users.noreply.github.com>
2023-10-31 20:08:25 -0300

    Update config.php

--- config.php
+++ config.php
@@ -2,6 +2,6 @@
 //root path to the directory;
 define('ABSPATH', dirname(__FILE__));
 //HTTP access;
-define('HOME_URI', 'http://'. $_SERVER['SERVER_NAME'] . '/dummie_passwordgen/');
+define('HOME_URI', 'http://'. $_SERVER['SERVER_NAME'] . '/PasswordGen/');
 
 ?>
